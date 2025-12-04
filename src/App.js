d5f6954e747f4c6ff2e254df2218f9b74c22d005
PIerog1 <p1erogbizn3s@gmail.com>
2025-12-04 10:12:22 +0100

    7.2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import DefaultValue from './components/71';
+import DefaultValue2 from './components/72';
 
 function App() {
   return (
     <>
-    <DefaultValue/>
+    <DefaultValue2/>
     </>
   );
 }
